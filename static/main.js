4675055e22c53331b27af7d6ea73ad96b8a0c405
parkjei <parkjei94@gmail.com>
2023-05-18 01:07:05 +0900

    오류 코드 수정 및 주석 추가

--- static/main.js
+++ static/main.js
@@ -13,9 +13,12 @@ function save_comment() {
   formData.append("comment_give", comment);
 
   // 입력되었는지 확인해주는 기능
+  // <-----------빈 값 검사_제이-----------------
+  // 입력 폼에 값을 입력하지 않았을 경우 알럿 노출
   if (!$("#name").val()) return alert("작성자 이름을 입력해주세요");
   if (!$("#password").val()) return alert("비밀번호를 입력해주세요");
   if (!$("#comment").val()) return alert("응원 한마디를 입력해주세요");
+  // --------------------제이----------------------->
 
   fetch("/comment", { method: "POST", body: formData })
     .then((res) => res.json())
@@ -37,10 +40,10 @@ function show_comment() {
         let id = a["id"];
         let password = a["password"];
 
+        // <-----------// 삭제 아이콘 추가(폰트어썸), 삭제 버튼 클릭 시 비밀번호 입력 모달창 노출_제이--------------->
         let temp_html = `<div class="card">
                       <div class="card-body">
                       <div class="icon-btn">
-                      <a href=""><i class="fa-regular fa-pen-to-square"></i></a>
                       <a href="" data-bs-toggle="modal" data-bs-target="#DelModal" onclick="delEvents('${id}')"><i class="fa-solid fa-trash-can"></i>
                       </a>
                       </div>
@@ -54,15 +57,19 @@ function show_comment() {
       });
     });
 }
+// <------------------제아-----------------------
 function delDocument(id) {
   const password = $(".modal-pw").val();
 
+  // 삭제 모달창 input 빈값 검사
   if (!password) return alert("비밀번호를 입력해주세요");
 
   let formData = new FormData();
   formData.append("id", id);
   formData.append("password", password);
 
+  // 서버에서 가져온 값, (data["result"]) 고유 아이디, 유저가 입력한 비밀번호 값을 매치하여 일치할 경우(1) 삭제+알럿 노출,
+  // 일치하지 않을 경우(0) 삭제X+알럿 노출
   fetch("/delDocument", { method: "POST", body: formData })
     .then((res) => res.json())
     .then((data) => {
@@ -75,9 +82,13 @@ function delDocument(id) {
     });
 }
 
+// 버튼 이벤트
 function delEvents(id) {
   $("#del-btn").attr("onclick", `delDocument('${id}')`);
 }
+// attr - 엘리먼트의 속성 값을 가져오거나 변경할 수 있는 함수
+// del-btn 온클릭, delDocument의 아이디 값을 가져온다.
+// ----------------------제이----------------------->
 
 // ------------------------------------------------ 혜진
 // 모달 클래스(모달 내용) 가져오기
